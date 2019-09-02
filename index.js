a4f3bdea961977fb62bfb37437366ace341dd0e9
Sameer Desai <48178078+sameerdesai123@users.noreply.github.com>
2019-09-02 19:25:09 +0530

    updated index.js

--- index.js
+++ index.js
@@ -1,4 +1,9 @@
-  var UIController = function() {
+    var total, payment, b = 0.00;
+    var database = firebase.database();
+
+if (window.location.href == "https://jaltrack.firebaseapp.com/") 
+{
+    var UIController = function() {
         var UIComponents = {
         exit_flag : document.getElementById("exit-btn"),
         Bill : document.querySelector('.bill')
@@ -6,66 +11,75 @@
     return {
         UiCom : UIComponents,
         Ui_display : function(b) {
-            console.log("Entering Ui_display()");
             $('.bill').text(b);
         }   
     }
-  }();
-
-  var FlowController = function(UI) {
-    var flag = false, bill;
-    UI.UiCom.exit_flag.addEventListener('click', function() {
-        flag = true;
-    })
+    }();
+    
+    var FlowController = function(UI) {
+    var bill;
     function start() {
         $('.ls').text('0.00');
         $('.total').text('0.00');
         $('.bill').text('0.00');
         // redirect to completion page
     }
-
+    
     return {
         Initialize : function() {
-            // Clear UI
             start();
         },
         runControl : function() {
-            console.log("Entering runControl()");
             bill = startBilling();
-            console.log("Calling Ui_display()");
             UI.Ui_display(bill);
         }
     }
-  }(UIController);
-
-
-  
+    }(UIController);
     
-  var total, payment, b = 0.00;
-  var database = firebase.database();
-  var lsRef = firebase.database().ref('ls');
-  var totalRef = firebase.database().ref('total');
-  var bill_const = 2.0;
-
+    var bill_const = 2.0;
+    
             //var f = false;
-lsRef.on('value', function (snapshot) {
+    database.ref('ls').on('value', function (snapshot) {
      // This snapshot returns the value of total litres/seconds automatically
     $('.ls').text(snapshot.val());
-});
-totalRef.on('value', function (snapshot) {
+    });
+    database.ref('total').on('value', function (snapshot) {
     // This snapshot returns the value of total litres automatically
     $('.total').text(snapshot.val());
     total = parseFloat(document.getElementById('01').innerHTML);
     console.log(total);
     FlowController.runControl();
-
-});
-
+ //   printCharts(2);
+    
+    });
+    
     function startBilling() {
         console.log("Entering startBilling()");
             b = total*bill_const;
             console.log(b);
             return b;
     }
+    
+    FlowController.Initialize();
+}
 
-  FlowController.Initialize();
+
+// Js for signin page
+
+else if( window.location.href == "https://jaltrack.firebaseapp.com/signin.html") {
+    document.getElementById('submit').addEventListener('click', function() {
+        var user = document.getElementById('inputEmail').innerHTML;
+        var pass = document.getElementById('inputPassword').innerHTML;
+        var head = document.getElementById('head');
+        if ( database.ref(user) && database.ref(user).child(password) == pass) {
+            database = null;
+            database =firebase.database().ref(user); 
+            window.location="https://jaltrack.firebaseapp.com/";
+        }
+        else{
+            head.innerHTML = "WRONG CREDENTIALS TRY AGAIN!!";
+        }
+    });
+}
+
+
